91e2b1cf6c693bff342ee1a0fbe83c7a5544e9da
aqeeluser404 <aqeelhanslo@gmail.com>
2024-02-02 12:25:09 +0200

    added the wrapper container onto each of the pages

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="main-container">
-        <div class="container-one line-height-low">
-            <div>
-                <h1 class="font-l color-black family-koulen">About Me</h1>
-                <h1 class="font-m color-blue family-lekton">Sample Text</h1>
+    <div class="wrapper-container">
+        <div class="main-container">
+            <div class="container-one line-height-low">
+                <div>
+                    <h1 class="font-l color-black family-koulen">About Me</h1>
+                    <h1 class="font-m color-blue family-lekton">Sample Text</h1>
+                </div>
+                <div>
+                    <p class="font-s color-black family-plus-code">My name is Aqeel Hanslo, third year graduate at CPUT, I have chosen the career in Applications Development because I not only enjoy building applications but also consider it one of my strong suits. I possess the ability to think outside the box to create innovative solutions for problems.</p>
+                    <br><br>
+                    <button class="button">Download CV</button>
+                </div>
             </div>
-            <div>
-                <p class="font-s color-black family-plus-code">My name is Aqeel Hanslo, third year graduate at CPUT, I have chosen the career in Applications Development because I not only enjoy building applications but also consider it one of my strong suits. I possess the ability to think outside the box to create innovative solutions for problems.</p>
-                <br><br>
-                <button class="button">Download CV</button>
+            <div class="container-two">
+                <h1 class="font-l color-blue family-koulen">Education</h1>
+
+            </div>
+            <div class="page-number">
+                <h1 class="font-xs color-blue family-lekton">02</h1>
             </div>
         </div>
-        <div class="container-two">
-            <h1 class="font-l color-blue family-koulen">Education</h1>
-
-        </div>
-
-       
-
-    </div>
-    <div class="page-number">
-        <h1 class="font-xs color-blue family-lekton">02</h1>
     </div>
 </template>
 
