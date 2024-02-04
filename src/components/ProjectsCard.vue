58e95e155d42d95d967fe834873b42d6f2c4e2f9
aqeeluser404 <aqeelhanslo@gmail.com>
2024-02-04 21:50:02 +0200

    Started implementing the projects section

--- src/components/ProjectsCard.vue
+++ src/components/ProjectsCard.vue
@@ -1,7 +1,61 @@
 <template>
     <div class="wrapper-container">
         <div class="main-container">
-            <h1>Projects</h1>
+            
+            <div class="text-container line-height-low">
+                <h1 class="font-l color-white family-koulen">Projects</h1>
+                <h1 class="font-m color-blue family-lekton">Sample Text</h1>
+            </div>
+
+            <div class="projects-container">
+                <div class="container">
+                    <!-- project 1 -->
+                    <input type="radio" name="slide" id="c1" checked>
+                    <label for="c1" class="card">
+                        <div class="row">
+                            <div class="icon">1</div>
+                            <div class="description">
+                                <h1>Project 1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </div>
+                    </label>
+                    <!-- project 2 -->
+                    <input type="radio" name="slide" id="c1" checked>
+                    <label for="c1" class="card">
+                        <div class="row">
+                            <div class="icon">2</div>
+                            <div class="description">
+                                <h1>Project 2</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </div>
+                    </label>
+                    <!-- project 3 -->
+                    <input type="radio" name="slide" id="c1" checked>
+                    <label for="c1" class="card">
+                        <div class="row">
+                            <div class="icon">3</div>
+                            <div class="description">
+                                <h1>Project 3</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </div>
+                    </label>
+                    <!-- project 4 -->
+                    <input type="radio" name="slide" id="c1" checked>
+                    <label for="c1" class="card">
+                        <div class="row">
+                            <div class="icon">4</div>
+                            <div class="description">
+                                <h1>Project 4</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                        </div>
+                    </label>
+                </div>
+            </div>
+
             <div class="page-number">
                 <h1 class="font-xs color-blue family-lekton">04</h1>
             </div>
@@ -20,8 +74,46 @@ export default {
         background-color: black;
     }
     .main-container {
-        flex-direction: row;
         align-items: center;
         justify-content: center;
+    }
+    .text-container {
+        display: flex;
+        flex-direction: column;
+    }
+    /* projects */
+    .projects-container {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .container {
+        height: 400px;
+        display: flex;
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+    }
+    .card {
+        width: 80px;
+        border-radius: .75rem;
+        background-size: cover;
+        cursor: pointer;
+        overflow: hidden;
+        border-radius: 2rem;
+        margin: 0 10px;
+        display: flex;
+        transition: .6s cubic-bezier(.28, -0.03, 0, .99);
+        box-shadow: 0px 10px 30px -5px rgba(0,0,0,0.8);
+    }
+
+    .card > .row {
+        color: rgb(0, 0, 0);
+        display: flex;
+        flex-wrap: nowrap;
+    }
+    .card > .row > .icon {
+        
     }
 </style>
