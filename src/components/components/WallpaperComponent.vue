d4e624fa624e370b85511d808e11922d010de4a0
aqeeluser404 <aqeelhanslo@gmail.com>
2024-02-05 12:35:47 +0200

    slowed wallpaper video speed

--- src/components/components/WallpaperComponent.vue
+++ src/components/components/WallpaperComponent.vue
@@ -1,13 +1,28 @@
 <template>
-    <video autoplay loop muted class="video">
+    <video  ref="videoRef" autoplay loop muted class="video">
         <source src="@/assets/wallpaper/video-wallpaper.mp4" type="video/mp4"/> 
     </video>
 </template>
 
 <script>
 export default {
-    name : 'WallpaperComponent'
-}
+    name : 'WallpaperComponent',
+    mounted() {
+        this.slowDownVideo();
+    },
+    methods: {
+        slowDownVideo() {
+            const video = this.$refs.videoRef;
+
+            if ('playbackRate' in video) {
+                video.style.transition = 'all 0.5s ease-in-out';
+                video.playbackRate = 0.4; 
+            } else {
+                console.error('Playback rate adjustment not supported.');
+            }
+        }
+    }
+};
 </script>
 
 <style scoped>
